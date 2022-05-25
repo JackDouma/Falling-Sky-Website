db6ded3d078df4efe97c890eb73b6efb20dbb23b
JackDouma <jackdouma0@gmail.com>
2022-05-25 19:24:58 -0400

    responsive

--- includes/header.php
+++ includes/header.php
@@ -26,73 +26,34 @@
                     <img id="logo" src="img\logo.png">
                     <a class="left-links" href="index.php">Home</a>
                     <a class="left-links" href="rocket-league.php">Teams</a>
-                    <a class="left-links" href="about-the-staff.php">About the Staff</a>
-                    <a class="left-links" href="join-the-staff.php">Join the Staff</a>
+                    <a class="left-links" href="staff.php">Staff</a>
                     <a class="left-links" href="https://realgamerwear.net/teams/falling-sky-esports/" target="_blank">Merch</a>
-                </li>
-                       
-                <!-- This php code only adds the registered admins to the header if user if logged in-->
-                <?php
-                    if (session_status() == PHP_SESSION_NONE)
-                    {
-                        session_start();
-                    }
-                ?>
-            </ul>
-
-            <!-- right side of header -->
-            <ul class="navbar-nav ms-auto">
-                <!-- This php code will allow the user to login as either admin or captain-->
-                <!-- If the user is already logged in as admin, it will display logging out, and name of the username -->
-                <?php
-                    // not logged in as either captain or admin
-                    if(empty($_SESSION['captainName']) &&(empty($_SESSION['adminName'])))
-                    {
-                        echo 
-                        '<li class="nav-item">
-                            <a class="nav-link" href="admin-login.php">Admin Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="captain-login.php">Captain Login</a>
-                        </li>';
-                    }
-                    // logged in as admin
-                    else if(!empty($_SESSION['adminName']))
-                    {
-                        // in walter white voice "say my name", create player, team, admin, captain, logout. This will be put into seperate drop downs at some point
-                        echo 
-                        '<li class="nav-item">
-                            <a class="nav-link" href="#">' . $_SESSION['adminName'] . '</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="add-igl-player.php">Create Player</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="add-igl-team.php">Create Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="add-admin.php">Create Admin</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="add-captain.php">Create Captain</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                        </li>';
-                    }
-                    // logged in as captain
-                    else if(!empty($_SESSION['captainName']))
-                    {
-                        // in walter white voice "say my name" 2) logout
-                        echo 
-                        '<li class="nav-item">
-                            <a class="nav-link" href="#">' . $_SESSION['captainName'] . '</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="logout.php">Logout</a>
-                        </li>';
-                    }
-                ?>
-            </ul>
-                
+                    <?php
+                        if (session_status() == PHP_SESSION_NONE)
+                        {
+                            session_start();
+                        }
+                        // not logged in as either captain or admin
+                        if(empty($_SESSION['captainName']) &&(empty($_SESSION['adminName'])))
+                        {
+                            echo 
+                            '<a class="left-links" href="admin-login.php">Admin Login</a>
+                            <a class="left-links" href="captain-login.php">Captain Login</a>';
+                        }
+                        // logged in as admin
+                        else if(!empty($_SESSION['adminName']))
+                        {
+                            echo 
+                            '<a class="left-links" href="settings.php">Settings</a>                                                                            
+                            <a class="left-links" href="logout.php">Logout</a>';
+                        }
+                        // logged in as captain
+                        else if(!empty($_SESSION['captainName']))
+                        {
+                            echo 
+                            '<a class="left-links" href="logout.php">Logout</a>';
+                        }
+                    ?>
+                </li>                   
+            </ul>               
         </nav>
---
+++ settings.php
@@ -0,0 +1,21 @@
+<?php
+    $title = 'Settings';
+    require 'includes/header.php';
+    require 'includes/admin-auth.php';
+?>
+
+<a class="nav-link" href="add-igl-player.php">Create Player</a>
+<a class="nav-link" href="edit-igl-player.php">Edit Player</a>
+<a class="nav-link" href="add-igl-team.php">Create Team</a>
+<a class="nav-link" href="edit-igl-team.php">Edit Team</a>
+<a class="nav-link" href="add-igl-season.php">Create Season</a>
+<a class="nav-link" href="add-admin.php">Create Admin</a>
+<a class="nav-link" href="add-captain.php">Create Captain</a>
+<a class="nav-link" href="add-igl-team.php">Create Main RL Player</a>
+<a class="nav-link" href="edit-igl-team.php">Edit Main RL Player</a>
+
+</body>
+<?php
+    require 'includes/footer.php';
+?>
+</html>
