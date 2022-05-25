b9975e25b84d056e8bdf5f4b71c055d55ac3e6c0
JackDouma <jackdouma0@gmail.com>
2022-05-25 15:26:12 -0400

    header, footer, index css

--- includes/header.php
+++ includes/header.php
@@ -2,22 +2,18 @@
 <html lang="en">
 
     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap');
-        </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="This is the official website for Falling Sky Esports">
+        <meta name="robots" content="noindex, nofollow">
+        <link href="https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap" rel="stylesheet">
 
         <title>Falling Sky | <?php echo $title; ?></title>
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-        
+    
         <!-- css -->
         <link rel="stylesheet" href="styles.css">
-        
         <!-- bootstrap -->
         <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
-
         <!-- java script -->
         <script src="js/scripts.js" type="text/javascript" defer></script>
     </head>
@@ -25,87 +21,78 @@
     <!-- body start -->
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid">
-                <!-- logo 
-                <img src="./img/logo.png" alt="our logo"> -->
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <!-- left side of header -->
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav"> 
-                        <li class="nav-item">
-                            <img id="navimg" src="img\FSE1.png">
-                            <a class="left-links" href="index.php"><b>HOME</b></a>
-                            <a class="left-links" href="about-the-staff.php"><b>STAFF</b></a>
-                            <a class="left-links" href="https://realgamerwear.net/teams/falling-sky-esports/"><b>MERCH</b></a>
-                            <a class="left-links" href="contact.php"><b>CONTACT</b></a>
-                        </li>
+            <ul class="navbar-nav"> 
+                <li class="nav-item">
+                    <img id="logo" src="img\logo.png">
+                    <a class="left-links" href="index.php">Home</a>
+                    <a class="left-links" href="rocket-league.php">Teams</a>
+                    <a class="left-links" href="about-the-staff.php">About the Staff</a>
+                    <a class="left-links" href="join-the-staff.php">Join the Staff</a>
+                    <a class="left-links" href="https://realgamerwear.net/teams/falling-sky-esports/" target="_blank">Merch</a>
+                </li>
                        
-                        <!-- This php code only adds the registered admins to the header if user if logged in-->
-                        <?php
-                            if (session_status() == PHP_SESSION_NONE)
-                            {
-                                session_start();
-                            }
-                        ?>
-                    </ul>
+                <!-- This php code only adds the registered admins to the header if user if logged in-->
+                <?php
+                    if (session_status() == PHP_SESSION_NONE)
+                    {
+                        session_start();
+                    }
+                ?>
+            </ul>
 
-                    <!-- right side of header -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- This php code will allow the user to login as either admin or captain-->
-                        <!-- If the user is already logged in as admin, it will display logging out, and name of the username -->
-                        <?php
-                            // not logged in as either captain or admin
-                            if(empty($_SESSION['captainName']) &&(empty($_SESSION['adminName'])))
-                            {
-                                echo 
-                                '<li class="nav-item">
-                                    <a class="nav-link" href="admin-login.php">Admin Login</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="captain-login.php">Captain Login</a>
-                                </li>';
-                            }
-                            // logged in as admin
-                            else if(!empty($_SESSION['adminName']))
-                            {
-                                // in walter white voice "say my name", create player, team, admin, captain, logout. This will be put into seperate drop downs at some point
-                                echo 
-                                '<li class="nav-item">
-                                    <a class="nav-link" href="#">' . $_SESSION['adminName'] . '</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="add-igl-player.php">Create Player</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="add-igl-team.php">Create Team</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="add-admin.php">Create Admin</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="add-captain.php">Create Captain</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="logout.php">Logout</a>
-                                </li>';
-                            }
-                            // logged in as captain
-                            else if(!empty($_SESSION['captainName']))
-                            {
-                                // in walter white voice "say my name" 2) logout
-                                echo 
-                                '<li class="nav-item">
-                                    <a class="nav-link" href="#">' . $_SESSION['captainName'] . '</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="logout.php">Logout</a>
-                                </li>';
-                            }
-                        ?>
-                    </ul>
-                </div>
-            </div>
+            <!-- right side of header -->
+            <ul class="navbar-nav ms-auto">
+                <!-- This php code will allow the user to login as either admin or captain-->
+                <!-- If the user is already logged in as admin, it will display logging out, and name of the username -->
+                <?php
+                    // not logged in as either captain or admin
+                    if(empty($_SESSION['captainName']) &&(empty($_SESSION['adminName'])))
+                    {
+                        echo 
+                        '<li class="nav-item">
+                            <a class="nav-link" href="admin-login.php">Admin Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="captain-login.php">Captain Login</a>
+                        </li>';
+                    }
+                    // logged in as admin
+                    else if(!empty($_SESSION['adminName']))
+                    {
+                        // in walter white voice "say my name", create player, team, admin, captain, logout. This will be put into seperate drop downs at some point
+                        echo 
+                        '<li class="nav-item">
+                            <a class="nav-link" href="#">' . $_SESSION['adminName'] . '</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="add-igl-player.php">Create Player</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="add-igl-team.php">Create Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="add-admin.php">Create Admin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="add-captain.php">Create Captain</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>';
+                    }
+                    // logged in as captain
+                    else if(!empty($_SESSION['captainName']))
+                    {
+                        // in walter white voice "say my name" 2) logout
+                        echo 
+                        '<li class="nav-item">
+                            <a class="nav-link" href="#">' . $_SESSION['captainName'] . '</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>';
+                    }
+                ?>
+            </ul>
+                
         </nav>
---
+++ join-the-staff.php
@@ -0,0 +1,10 @@
+<?php
+    $title = 'Join the Staff';
+    require 'includes/header.php';
+?>
+
+</body>
+<?php
+    require 'includes/footer.php';
+?>
+</html>
