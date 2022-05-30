5f611e9712b4232b9b32b11f49750f797040aa1b
JackDouma <jackdouma0@gmail.com>
2022-05-30 16:06:04 -0400

    back n' front end

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,5 @@
 <?php
-// global db connection for all pages
-$db = new PDO('mysql:host=;dbname=', '', '');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // global db connection for all pages
+    $db = new PDO('mysql:host=localhost;dbname=u717617285_FSE', 'u717617285_FSE', 'd4#IsPB/');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
